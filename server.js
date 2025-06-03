310a2282a3f2965f48b7824985fb95f2c4997172
huzaifa <huzaifafareed817@gmail.com>
2025-06-02 22:04:34 -0400

    Update server.js
    connected database
    connected to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true} )
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+// keep going 6:30
 
 app.listen(3000, () => console.log('server on'))
 
