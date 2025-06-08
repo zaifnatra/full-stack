f62ad99c9ab7bf4abc9781aa4acedff54a4ee95b
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-06-08 11:49:07 -0400

    Update users.js
    Added data type and values

--- models/users.js
+++ models/users.js
@@ -2,8 +2,23 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: {
+        type: String,
+        required:true
+    },
+
+    userSubscription: {
+        type: String,
+        required: true
+    },
+
+    userSubscriptionDate:{
+        type: Date,
+        required: true,
+        default: Date.now
     }
 
     
     
 })
+
+module.exports = mongoose.model('User', userSchema);
